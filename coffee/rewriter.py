3243230a3d3331461e58109a39f3dda2c296cd99
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-06-19 13:39:49 +0100

    Fix exansions tracking in expander

--- coffee/rewriter.py
+++ coffee/rewriter.py
@@ -659,13 +659,12 @@ class ExpressionExpander(object):
         self.expanded_decls = {}
         self.cache = {}
 
-    def _hoist(self, expansion):
+    def _hoist(self, expansion, info):
         """Try to aggregate an expanded expression E with a previously hoisted
         expression H. If there are no dependencies, H is expanded with E, so
         no new symbols need be introduced. Otherwise (e.g., the H temporary
         appears in multiple places), create a new symbol."""
         exp, grp = expansion.left, expansion.right
-        cache_key = (str(exp), str(grp))
 
         # First, check if any of the symbols in /exp/ have been hoisted
         try:
@@ -679,6 +678,7 @@ class ExpressionExpander(object):
         # Before moving on, access the cache to check whether the same expansion
         # has alredy been performed. If that's the case, we retrieve and return the
         # result of that expansion, since there is no need to add further temporaries
+        cache_key = (str(exp), str(grp))
         if cache_key in self.cache:
             return {exp: self.cache[cache_key]}
 
@@ -694,8 +694,8 @@ class ExpressionExpander(object):
         grp_syms = SymbolReferences().visit(grp, ret=retval).keys()
         for l in reversed(self.expr_info.loops):
             for g in grp_syms:
-                g_refs = self.info['symbol_refs'][g]
-                g_deps = set(flatten([self.info['symbols_dep'][r[0]] for r in g_refs]))
+                g_refs = info['symbol_refs'][g]
+                g_deps = set(flatten([info['symbols_dep'].get(r[0], []) for r in g_refs]))
                 if any([l.dim in g.dim for g in g_deps]):
                     return {}
             if l in hoisted_place.children:
@@ -716,6 +716,7 @@ class ExpressionExpander(object):
                                                    'i': len(self.expanded_decls)}
         decl = dcopy(hoisted_decl)
         decl.sym.symbol = hoisted_exp.symbol
+        decl.scope = LOCAL
         stmt = Assign(hoisted_exp, expr)
         # Update the AST
         hoisted_loop.body.append(stmt)
@@ -726,6 +727,16 @@ class ExpressionExpander(object):
         self.expr_graph.add_dependency(hoisted_exp, expr)
         self.cache[cache_key] = hoisted_exp
         return {exp: hoisted_exp}
+
+    def _build(self, exp, grp):
+        """Create a node for the expansion and keep track of it."""
+        expansion = Prod(exp, dcopy(grp))
+        # Track the new expansion
+        self.expansions.append(expansion)
+        # Untrack any expansions occured in children nodes
+        if grp in self.expansions:
+            self.expansions.remove(grp)
+        return expansion
 
     def _expand(self, node, parent):
         if isinstance(node, Symbol):
@@ -754,9 +765,8 @@ class ExpressionExpander(object):
             expandable = r_exps if l_type == self.GROUP else l_exps
             to_replace = defaultdict(list)
             for exp, grp in itertools.product(expandable, groupable):
-                expansion = Prod(exp, dcopy(grp))
+                expansion = self._build(exp, grp)
                 to_replace[exp].append(expansion)
-                self.expansions.append(expansion)
             ast_replace(node, {k: ast_make_expr(Sum, v) for k, v in to_replace.items()},
                         mode='symbol')
             # Update the parent node, since an expression has just been expanded
@@ -781,18 +791,18 @@ class ExpressionExpander(object):
         """Perform the expansion of the expression rooted in ``self.stmt``.
         Symbols for which the lambda function ``should_expand`` returns
         True are expansion candidates."""
-        # Preload and set data structures for expansion
         self.expansions = []
         self.should_expand = should_expand
-        self.info = visit(self.expr_info.outermost_loop, info_items=['symbol_refs',
-                                                                     'symbols_dep'])
+
+        info = visit(self.expr_info.outermost_loop,
+                     info_items=['symbol_refs', 'symbols_dep'])
 
         # Expand according to the /should_expand/ heuristic
         self._expand(self.stmt.children[1], self.stmt)
 
         # Try to aggregate expanded terms with hoisted expressions (if any)
         for expansion in self.expansions:
-            hoisted = self._hoist(expansion)
+            hoisted = self._hoist(expansion, info)
             if hoisted:
                 ast_replace(self.stmt, hoisted, copy=True, mode='symbol')
                 ast_remove(self.stmt, expansion.right, mode='symbol')
