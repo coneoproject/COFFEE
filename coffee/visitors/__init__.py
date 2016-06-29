a5dc72a7cd7d3663cd711bb12b8fce80e5a9b99e
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-06-29 15:32:22 +0100

    fix flake8

--- coffee/visitors/__init__.py
+++ coffee/visitors/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
-from coffee.visitors.utilities import *
-from coffee.visitors.inspectors import *
+from coffee.visitors.utilities import *  # noqa
+from coffee.visitors.inspectors import *  # noqa
