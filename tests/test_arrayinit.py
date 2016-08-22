48e2f8041591d586c4f596cbdeaefddddde520e6
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-08-22 14:56:05 +0100

    Add test

---
+++ tests/test_arrayinit.py
@@ -0,0 +1,8 @@
+from coffee import base as ast
+import numpy as np
+
+
+def test_funcall_in_arrayinit():
+    tree = ast.ArrayInit(np.asarray([ast.FunCall("foo"), ast.Symbol("bar")]))
+
+    assert tree.gencode() == "{foo(), bar}"
