bb7090b98fcd68d359982b3d1b110ada175a66f5
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-09-19 09:47:26 +0100

    Add some code printing precedence tests

---
+++ tests/test_precedence.py
@@ -0,0 +1,13 @@
+from coffee import base as ast
+
+
+def test_prod_div():
+    tree = ast.Prod("a", ast.Div("1", "b"))
+
+    assert tree.gencode() == "a * (1 / b)"
+
+
+def test_unary_op():
+    tree = ast.Not(ast.Or("a", ast.And("b", "c")))
+
+    assert tree.gencode() == "!(a || (b && c))"
