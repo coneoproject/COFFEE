ecd02c1755ed6e68a97a4c6be7f5c1afd1bff07c
NicholasBermuda <snicholasbarton@gmail.com>
2017-08-09 18:40:13 +0200

    update test to reflect new parens in binops

--- tests/test_precedence.py
+++ tests/test_precedence.py
@@ -6,10 +6,10 @@ from coffee import base as ast
 def test_prod_div():
     tree = ast.Prod("a", ast.Div("1", "b"))
 
-    assert tree.gencode() == "a * (1 / b)"
+    assert tree.gencode() == "(a) * (((1) / (b)))"
 
 
 def test_unary_op():
     tree = ast.Not(ast.Or("a", ast.And("b", "c")))
 
-    assert tree.gencode() == "!(a || (b && c))"
+    assert tree.gencode() == "!((a) || (((b) && (c))))"
